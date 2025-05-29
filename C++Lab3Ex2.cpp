831cda39d7e185974298fb2c4547d3133609f071
DSllide <dreatsllide@gmail.com>
2025-05-29 17:37:39 +0300

    Add files via upload

---
+++ C++Lab3Ex2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int m, n, k;
+
+    cout << "Enter number of rows (m), columns (n), and border thickness (k): ";
+    cin >> m >> n >> k;
+
+
+    if (k < 1 || k >= m || k >= n || m > 100 || n > 100) {
+        cout << "Invalid input!" << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+
+            if (i < k || i >= m - k || j < k || j >= n - k) {
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
